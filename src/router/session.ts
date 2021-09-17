17deabe6986a49bcd4504a9b1c742a8fab693701
祝传鹏 <Luomusha@gmail.com>
2021-09-17 22:41:24 +0800

    i

--- src/router/session.ts
+++ src/router/session.ts
@@ -5,16 +5,17 @@ import {findAuthenticationByIdentifier} from "../service/Authentication";
 import {setSession} from "../service/Session";
 
 const session = new Router<DefaultState, Context>()
-session.get('/session', async (ctx: Context, next: Next) => {
-    const {client_id, return_to} = ctx.request.query
-    let client
-    if (client_id && !Array.isArray(client_id)) {
-        client = await findClientById(client_id)
-    }
-    ctx.state = {client_id, client_secret: client && client?.key, client, return_to}
-    await ctx.render('session')
-    await next()
-})
+session
+    .get('/session', async (ctx: Context, next: Next) => {
+        const {client_id, return_to} = ctx.request.query
+        let client
+        if (client_id && !Array.isArray(client_id)) {
+            client = await findClientById(client_id)
+        }
+        ctx.state = {client_id, client_secret: client && client?.key, client, return_to}
+        await ctx.render('session')
+        await next()
+    })
     .post('/session', async (ctx: Context, next: Next) => {
         const {identifier, certificate, return_to, timestamp, timestamp_secret} = ctx.request.body
         const auth = await findAuthenticationByIdentifier(identifier, certificate)
