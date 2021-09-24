7fba6387dec4bb03d3417d847a80a471141a59a5
dhc-zhuchuanpeng <chuanpeng1.zhu@dhc.com.cn>
2021-09-25 02:08:28 +0800

    删除console.log，代替用logger

--- src/router/oauth.ts
+++ src/router/oauth.ts
@@ -9,13 +9,10 @@ import {logger} from "../common/logger";
 
 export const beforeGetAuthorize = async (ctx: Context, next: Next): Promise<void> => {
     const {client_id} = ctx.request.query
-    if (!client_id || Array.isArray(client_id)) {
-        throw Error();
-    }
+    ctx.assert(client_id, 400, 'client_id is required')
     const {userId} = ctx.state
     const userWithApplications = await findUserApplicationsById(userId)
     ctx.assert(userWithApplications, 401)
-    console.log(Array.isArray(userWithApplications.applications));
     if (!userWithApplications.applications.includes(client_id)) return ctx.redirect(`/application?client_id=${client_id}&return_to=${ctx.request.url}`)
     await next()
 }
