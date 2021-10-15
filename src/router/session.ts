34d5bc539bc7554163a0dcc4fe599f5ab6d5bcb1
dhc-zhuchuanpeng <chuanpeng1.zhu@dhc.com.cn>
2021-10-15 13:34:08 +0800

    登录错误跳转页面修复

--- src/router/session.ts
+++ src/router/session.ts
@@ -3,26 +3,32 @@ import {Context, DefaultState, Next} from "koa";
 import {findClientById} from "../service/Client";
 import {findAuthenticationByIdentifier} from "../service/Authentication";
 import {setSession} from "../service/Session";
+import {sessionErrorHandler} from "./middleware/error";
+
+const getSession = async (ctx: Context, next: Next) => {
+    const {client_id, return_to} = ctx.request.query
+    let client
+    if (client_id && !Array.isArray(client_id)) client = await findClientById(client_id)
+    ctx.state = {client_id, client_secret: client && client?.key, client, return_to}
+    await ctx.render('session')
+    await next()
+}
+
+const postSession = async (ctx: Context, next: Next) => {
+    const {identifier, certificate, return_to, client_id} = ctx.request.body
+    const query = new URLSearchParams({client_id, return_to})
+    const auth = await findAuthenticationByIdentifier(identifier, certificate)
+    // if (!auth) return ctx.redirect(`/session?${query}`)
+    ctx.assert(auth, 401)
+    const session = await setSession(auth.user.id)
+    ctx.cookies.set('user_session', session)
+    await ctx.redirect(return_to)
+    await next()
+}
 
 const session = new Router<DefaultState, Context>()
 session
-    .get('/session', async (ctx: Context, next: Next) => {
-        const {client_id, return_to} = ctx.request.query
-        let client
-        if (client_id && !Array.isArray(client_id)) client = await findClientById(client_id)
-        ctx.state = {client_id, client_secret: client && client?.key, client, return_to}
-        await ctx.render('session')
-        await next()
-    })
-    .post('/session', async (ctx: Context, next: Next) => {
-        const {identifier, certificate, return_to, client_id} = ctx.request.body
-        const query = new URLSearchParams({client_id, return_to})
-        const auth = await findAuthenticationByIdentifier(identifier, certificate)
-        if (!auth) return ctx.redirect(`/session?${query}`)
-        const session = await setSession(auth.user.id)
-        ctx.cookies.set('user_session', session)
-        await ctx.redirect(return_to)
-        await next()
-    })
+    .get('/session', getSession)
+    .post('/session', postSession, sessionErrorHandler)
 
 export default session
