7ca3d45dd0c73b267fd00acc5318632015a6b62d
lossa <luomusha@gmail.com>
2021-11-02 14:42:37 +0800

    update error in css

--- src/component/ErrorPage.tsx
+++ src/component/ErrorPage.tsx
@@ -13,16 +13,20 @@ const ErrorPage = ({status = 404, title = "404", message = "Page Not Found"}: Er
         <meta charSet="UTF-8"/>
         <title>{title}</title>
         <link rel="stylesheet" href="/assets/error.css"/>
+        <link rel="stylesheet" href="/style.css"/>
     </head>
-    <body>
-    <img src="/assets/decorate.svg" alt="svg"/>
-    <div className="message-box">
-        <h1>{status}</h1>
-        <p>{message}</p>
-        <div className="buttons-con">
-            <div className="action-link-wrap">
-                <a onClick={back} className="link-button link-back-button">返回上一页</a>
-                <a href="#" className="link-button">返回首页</a>
+    <body className="bg-gray-700">
+    <div className="m-auto max-w-7xl flex justify-around items-center text-white top-1/2 relative -mt-60">
+        <img src="/assets/decorate.svg"
+             className="flex-shrink"
+             alt="svg"/>
+        <div className="mr-20">
+            <h1 className="text-6xl mb-6">{status}</h1>
+            <p>{message}</p>
+            <div className="mt-10 text-sm">
+                <a onClick={back}
+                   className="cursor-pointer rounded-md px-6 py-2 bg-green-400 font-bold mr-2.5">返回上一页</a>
+                <a href="#" className="cursor-pointer rounded-md px-6 py-2 bg-green-400 font-bold">返回首页</a>
             </div>
         </div>
     </div>
