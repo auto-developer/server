f9140401ce14a6df982ef4f791080c1671745d37
lossa <luomusha@gmail.com>
2021-11-02 10:45:08 +0800

    update sign in css

--- src/component/SignIn.tsx
+++ src/component/SignIn.tsx
@@ -4,8 +4,8 @@ import {Client} from "../type";
 
 export type SignInProps = {
     client?: Client,
-    return_to:string,
-    client_id:string,
+    return_to: string,
+    client_id: string,
     error?: {
         message: string;
     }
@@ -18,24 +18,26 @@ const SignIn = (props: SignInProps) => <html lang="en">
     <link rel="stylesheet" href="/style.css"/>
 </head>
 <body>
+<div className="text-center w-full pt-8 pb-6">
+    <a href="">
+        <svg height="48" aria-hidden="true" viewBox="0 0 16 16" width="48" data-view-component="true"
+             className="inline-block">
+            <path fillRule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
+        </svg>
+    </a>
+</div>
 <main>
-    <div className="auth-form">
-        <div className="app-icon">
-            <svg height="48" aria-hidden="true" viewBox="0 0 16 16" width="48" data-view-component="true"
-                 className="octicon octicon-mark-github">
-                <path fillRule="evenodd"
-                      d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
-            </svg>
-
+    <div className="auth-form px-4">
+        <div className="mb-4 text-center text-2xl font-light">
+            <h1>Sign in to Github</h1>
         </div>
-        <h1>Sign in to Github</h1>
-        <div className="auth-form-body">
+        <div className="auth-form-body p-5 border-r-4">
             {props.client && <div className="auth-application">
               <div className="logo">
                 <img src={props.client.logo} alt="Application logo"/>
               </div>
-              <p>
-                Sign in to <strong>{props.client.name}</strong>
+              <p>Sign in to <strong>{props.client.name}</strong>
                 <br/>
                 to continue to <strong>GitLab.com</strong>
               </p>
@@ -60,6 +62,7 @@ const SignIn = (props: SignInProps) => <html lang="en">
         </p>
     </div>
 </main>
+
 </body>
 </html>
 
